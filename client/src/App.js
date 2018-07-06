ab2bdc8a71320bf65e646089d50a5eef0d74db1f
Sandra Peterson <writesandy@gmail.com>
2018-07-06 12:17:44 -0500

    more routes

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,28 @@ import TopArticles from "./components/TopArticles";
 import './App.css';
 
 class App extends Component {
+
+state = {
+   stSavedArticles = [],
+   articleSearch = "",
+};
+
+
+handleInputChange = event => {
+   const {name, value} = event.target;
+   this.setState({
+     [name]: value
+   });
+};
+
+handleFormSubmit = event => {
+  event.preventDefault();
+  API.getArticles(this.state.articleSearch)
+  .then(res => this.setState( {stSavedArticles: res.data} ) 
+  .catch(err => console.log(err));
+};
+
+
   render() {
     return (
       <div className="App">
