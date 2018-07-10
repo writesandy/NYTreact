8ac26137a965622dafbf38b7725e2b7fa41b90f1
Sandra Peterson <writesandy@gmail.com>
2018-07-09 21:21:16 -0500

    queryURL

--- client/src/components/TopArticles/TopArticles.js
+++ client/src/components/TopArticles/TopArticles.js
@@ -9,7 +9,7 @@ const TopArticles = () => (
   <div className="panel panel-primary">
 
     <div className="panel-heading">
-      <h3 className="panel-title"><strong><i className="fa fa-table"></i>   Top Articles</strong></h3>
+      <h3 className="panel-title"><strong><i className="fa fa-table"></i>Top Articles</strong></h3>
     </div>
     <div className="panel-body" id="well-section">
     </div>
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,42 @@
 const axios = require("axios");
 const router = require("express").Router();
 
+function buildQueryURL() {
+  // queryURL is the url we'll use to query the API
+  const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+  // add the api key parameter (the one we received when we registered)
+  queryURL += "?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
+  // grab text the user typed into the search input, add as parameter to url
+  const searchTerm = $("#search-term").val().trim();
+  queryURL += "&q=" + searchTerm;
+
+  // if the user provides a startYear, include it in the queryURL
+  const startYear = $("#start-year").val().trim();
+
+  if (parseInt(startYear)) {
+    queryURL += "&begin_date=" + startYear + "0101";
+  }
+
+  // if the user provides an endYear, include it in the queryURL
+  const endYear = $("#end-year").val().trim();
+
+  if (parseInt(endYear)) {
+    queryURL += "&end_date=" + endYear + "0101";
+  }
+
+  // Logging the URL so we have access to it for troubleshooting
+  console.log("---------------\nURL: " + queryURL + "\n---------------");
+
+  return queryURL;
+}
+
+
+
 router.get("/articles", (req, res) => {
   axios
-    .get("http://www.nytimes.com/", { params: req.query })
+    .get(queryURL, { params: req.query })
     .then(({ data: { results } }) => res.json(results))
     .catch(err => res.status(422).json(err));
 });
